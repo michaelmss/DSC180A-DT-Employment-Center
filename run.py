e737e89a42476aef32a732638007272a771891a6
Mike <mssmichaelsmith@gmail.com>
2023-12-11 13:58:35 -0800

    switched to bash file for convenience

--- run.py
+++
@@ -1,6 +0,0 @@
-from python import analysis
-from python import median_income_and_age
-from python import proximity_map
-from python import race_data_creation
-from python import replication_DTCenter
-from python import traveling_from
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Change to the directory containing the Python scripts
+cd ./python
+
+# Run the Python scripts
+python analysis.py
+python median_income_and_age.py
+python proximity_map.py
+python race_data_creation.py
+python replication_DTCenter.py
+python traveling_from.py
